227c689aacb9dda5659b58285501dc1b3b173d01
Humboldt123 <nonemunhsiv@gmail.com>
2020-05-24 23:05:46 -0400

    comit to master is epci

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 ./updateCommands.sh && ./updateStatic.sh 
-cd .. wget https://raw.githubusercontent.com/humboldt123/the-thunderhead/master/index.js
-wget
+cd .. && wget https://raw.githubusercontent.com/humboldt123/the-thunderhead/master/index.js
 node index.js
