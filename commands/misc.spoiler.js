2aa8c4b617f61eaf6fb8a720cd4307a6b878dd1c
Humboldt123 <nonemunhsiv@gmail.com>
2020-05-26 22:28:00 -0400

    add spoiler to test

---
+++ commands/misc.spoiler.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+
+module.exports.run = async (client, message, args) => {
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(client.msg["rejected_client_permission_MANAGE_MESSAGES"]);
+  if (!args[1]) return;
+  
+  for (var tagCheck in args) args[tagCheck] = args[tagCheck].replace(/||/g, '');
+
+  if (args[0] === "the") args = args.shift();
+  let book = args[0].toLowerCase().replace("scythe", "0").replace("thunderhead", "1").replace("toll", "2");
+  try {
+    let book = parseInt(book);
+  } catch (err) {
+    book = 3;
+  }
+
+  const sayMessage = args.shift().join(" ");
+  message.delete().catch(O_o => { }); // O_o - he vibes
+  message.channel.send(book + " " +sayMessage)
+  
+} 
+
+module.exports.config = {
+  name: "spoiler",
+  aliases: [],
+  use: "spoiler [Book] Spoiler",
+  description: "Mark a spoiler",
+  state : "gamma",
+  page: 1
+};
