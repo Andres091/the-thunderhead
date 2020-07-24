8772d42a0b3f82c63d5aa74ad24cbd5a2918f29c
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-24 16:29:34 -0400

    Start up miencraft server command

---
+++ commands/minecraftserver.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+const exec = require('child_process').exec;
+const config = require("../static/config.json");
+
+
+module.exports.run = async (client, message, args) => {
+  if (config.sudo.indexOf(message.author.id) < 0) return; // Dev Only
+
+    var code = "cd .. ; cd .. ; cd EventPaperServer ; ./start.sh";
+    if (args[0] && args[0] === "reset") code = "cd .. ; cd .. ; cd EventPaperServer ; ./reset.sh";
+    const logchannel = client.channels.cache.get("736317106877890632");
+    
+    if (message.author.bot) return;
+    const myShellScript = exec(code);
+    myShellScript.stdout.on('data', (data) => {
+       logchannel.send(`\`\`\`\n${data}\n\`\`\``);
+    });
+    myShellScript.stderr.on('data', (data) => {
+        logchannel.send(`\`\`\`\n${data}\n\`\`\``);
+    });
+};
+
+module.exports.config = {
+  name: `${config["developer_prepended_prefix"]}terminal`,
+  aliases: ["mcs"],
+  use: `${config["developer_prepended_prefix"]}terminal [Command]`,
+  description: "Start the minecraft server",
+  state: "delta",
+  page: 0
+};
