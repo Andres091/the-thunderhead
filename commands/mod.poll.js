b09896d7582e6e7c602e0b774eaeadab2d6aedd9
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-27 21:48:44 -0400

    Rewrote poll
    todo:
    add !args detection
    make it show what happens upon completion

--- commands/mod.poll.js
+++ commands/mod.poll.js
@@ -1,43 +1,74 @@
 const Discord = require("discord.js");
 const fs = require("graceful-fs");
+const ms = require('ms');
+const discordEmojiRegex = new RegExp("(<:.+?:\\d+>)")
+const unicodeEmojiRegex = new RegExp("(\\u00a9|\\u00ae|\[\\u2000-\\u3300\]|\\ud83c\[\\ud000-\\udfff\]|\\ud83d\[\\ud000-\\udfff\]|\\ud83e\[\\ud000-\\udfff\])")
 
-// This is a command template. I hope its self documenting so you don't have to sift through my god-awful code! ^.^
+module.exports.run = async(client, message, args) => {
+    let requiredPermission = "MANAGE_MESSAGES";
+    if (!message.member.hasPermission(requiredPermission)) return message.channel.send(client.msg["rejected_user_permission_" + requiredPermission]);
+    if (!message.guild.me.hasPermission(requiredPermission)) return message.channel.send(client.msg["rejected_client_permission_" + requiredPermission]);
+    requiredPermission = "EMBED_LINKS";
+    if (!message.member.hasPermission(requiredPermission)) return message.channel.send(client.msg["rejected_user_permission_" + requiredPermission]);
+    if (!message.guild.me.hasPermission(requiredPermission)) return message.channel.send(client.msg["rejected_client_permission_" + requiredPermission]);
 
-module.exports.run = async (client, message, args) => {
-  let requiredPermission = "MANAGE_MESSAGES"; 
-  if (!message.member.hasPermission(requiredPermission)) return message.channel.send(client.msg["rejected_user_permission_"+requiredPermission]);
-  if (!message.guild.me.hasPermission(requiredPermission)) return message.channel.send(client.msg["rejected_client_permission_"+requiredPermission]);
-   
+    var reactTimeLimit;
+    var argPoint;
+    var reactionsArray = []; //init array
+    var poll;
+
+    args.forEach(arg => {
+        try {
+            reactTimeLimit = ms(ms(arg), {
+                long: true
+            });
+            argPoint = args.indexOf(arg);
+        } catch {
+            if (unicodeEmojiRegex.test(arg)) {
+              if ((reactionsArray.indexOf(arg)) == -1) reactionsArray.push(arg); // some smartasses would put the emojis w/o a space
+            } else if (discordEmojiRegex.test(arg)) {
+              var discordEmoji = client.emojis.cache.get(arg.split(":")[2].replace(/[:<>]/g, ""))
+                if (discordEmoji != undefined) {
+                    if ((reactionsArray.indexOf(discordEmoji.id)) == -1) reactionsArray.push(discordEmoji.id);
+                }
+            }
+        }
+    });
+
+    poll = args.slice(argPoint + 1).join(" ");
 
 
-  if (!args[0])
-    return message.channel
-      .send(client.msg["poll_undefined"])
-      .then(message.channel.bulkDelete(1))
-      .then(msg =>
-        msg.delete({
-          timeout: 1000000000000
-        })
-      );
-  message.channel.bulkDelete(1);
+    let pollEmbed = new Discord.MessageEmbed()
+      .setColor(client.colors["discord"])
+      .setTitle("Poll")
+      .setDescription(poll)
+      .setFooter(`The poll will end ${reactTimeLimit} after initialization.`)
 
-  let pollEmbed = new Discord.MessageEmbed()
-    .setColor(client.colors["discord"])
-    .setTitle("Poll")
-    .setDescription(`${args}`.split(",").join(" "));
-  message.channel.send(pollEmbed)
-    .then(async function(msg) {
-      await msg.react("✅");
-      await msg.react("⛔");
+    const pollMessage = await message.channel.send(pollEmbed);
+    reactionsArray.forEach(async emoji => {
+      await pollMessage.react(emoji);
     })
-    .catch(function() {});
+
+
+
+  setTimeout(() => {
+      let neoPollEmbed = new Discord.MessageEmbed()
+      .setColor(client.colors["thunderhead"])
+      .setTitle("Poll Results")
+      .setDescription(poll) 
+      .setFooter("Results down below:")
+      pollMessage.edit(neoPollEmbed);
+  }, ms(args[argPoint]));
+
+// todo: actual poll result display
+
 };
 
 module.exports.config = {
-  name: "poll",
-  aliases: [],
-  use: "poll [Question]",
-  description: "Create a poll!",
-  state: "gamma",
-  page: 4
+    name: "poll",
+    aliases: [],
+    use: "poll [Emoji]*{However many emojis you want} [Time] [Poll]",
+    description: "Create a poll!",
+    state: "gamma",
+    page: 4
 };
