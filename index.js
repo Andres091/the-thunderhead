275d7f2e60f7fea03a0910e132e48e916b1f01fd
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-08-04 20:36:53 -0400

    fixed slur detection

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ client.on('message', async message => {
 	if(commandfile) commandfile.run(client, message, args);
 });
 
-const slursRegex = new RegExp("(nigg.{1,2}|tran(?![spfqcgze ]).{1,2}|fag.{1,2}t|r(?![ ]).{1,2}tar.{1})");
+const slursRegex = new RegExp("(nigg.{1,2}|tran(?![spfqcgze ]).{1,2}|fag.{1,2}t|retard)");
 
 
 client.on('messageUpdate', (oldMessage, newMessage) => {
